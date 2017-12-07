c109c7c19f3be7944df43bafe4e216c536980178
fcatania <felicatania@gmail.com>
2017-12-07 15:50:34 -0800

    implemented countNQueensSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -102,7 +102,37 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  let board = new Board({n: n});
+  let queenCount = 0;
+  let solveQueens = (rowIndex, n) => {
+    if (rowIndex === n) {
+      solutionCount++;
+      return true;
+    }
+    for (let i = 0; i < n; i++) {
+      board.togglePiece(rowIndex, i);
+      queenCount++;
+      if (board.hasAnyQueensConflicts()) {
+        board.togglePiece(rowIndex, i);
+        queenCount--;
+      } else {
+        if (solveQueens(rowIndex + 1, n)) {
+          board.togglePiece(rowIndex, i);
+          queenCount--;
+        } else {
+          return;
+        }
+      }
+    }
+    // finish the column loop
+    if (queenCount !== n) {
+      // backtrack to prev call
+      return true;
+    }
+  };
+
+  solveQueens(0, n);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
